b405f57e42a0ac4849be0ec4597902651ba8bbe8
daveusa31 <41593484+daveusa31@users.noreply.github.com>
2020-03-01 22:02:56 +0300

    My version

---
+++ config.py
@@ -0,0 +1,12 @@
+qiwi_number = "" #номер киви
+qiwi_token = "" #токен киви
+
+ADMIN_ID =   #админ айди
+TOKEN = ""  #токен тг бота
+
+price = 10
+test_price = 1
+
+
+start_message = "Текст\nСюда введите своё сообщение"
+
---
+++ functions.py
@@ -0,0 +1,34 @@
+def get_payment_link(sum,  number, comment):
+	link = "https://qiwi.com/payment/form/99?extra%5B%27account%27%5D={}&amountInteger={}&amountFraction=0&extra%5B%27comment%27%5D={}&currency=643&blocked[0]=sum&blocked[1]=comment&blocked[2]=account"
+	link = link.format(number, sum, comment)
+	return link
+
+def message_logger(text, username):
+	# Файл уже создан
+	try:
+		with open("log.txt", "a", encoding="utf-8") as f:
+			f.write(text + " | @" + username + "\n")
+
+	# Создаём файл
+	except:
+		with open("log.txt", "w", encoding="utf-8") as f:
+			f.write(text + " | @" + username + "\n")
+
+def get_all_users(file="base.txt"):
+	with open(file, "r", encoding="utf-8") as f:
+		users = f.read()
+
+	users = users.split("\n")
+
+	return users
+
+
+def get_buyers(file="pays_base.txt"):
+	with open(file, "r", encoding="utf-8") as f:
+		users = f.read()
+
+	users = users.split("\n")
+
+	return users
+
+	
